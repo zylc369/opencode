7ebabbe84cf19dfaccfc7bff3afe7d2002e154f6
buwai <buwai.bw@qq.com>
2026-03-05 22:18:27 +0800

    allow select in code label

--- packages/app/src/pages/layout.tsx
+++ packages/app/src/pages/layout.tsx
@@ -2047,7 +2047,7 @@ export default function Layout(props: ParentProps) {
   }
 
   return (
-    <div class="relative bg-background-base flex-1 min-h-0 flex flex-col select-none [&_input]:select-text [&_textarea]:select-text [&_[contenteditable]]:select-text">
+    <div class="relative bg-background-base flex-1 min-h-0 flex flex-col select-none [&_input]:select-text [&_textarea]:select-text [&_code]:select-text [&_[contenteditable]]:select-text">
       <Titlebar />
       <div class="flex-1 min-h-0 flex">
         <nav
